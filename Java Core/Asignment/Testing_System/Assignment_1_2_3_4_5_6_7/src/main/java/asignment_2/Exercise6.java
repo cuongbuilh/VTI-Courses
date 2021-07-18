5630f7a47b238844d4df58568be6762892995b32
cuongbuilh <cuongbuilh@gmail.com>
2021-07-18 23:26:08 +0700

    update test2_ex6

---
+++ Java Core/Asignment/Testing_System/Assignment_1_2_3_4_5_6_7/src/main/java/asignment_2/Exercise6.java
@@ -0,0 +1,36 @@
+package asignment_2;
+
+import entity.Account;
+import entity.Group;
+
+import java.util.Date;
+
+public class Exercise6 {
+    public static void main(String[] args) {
+        Account acc1 = new Account(1, "a@gmail.com", "a", "nva", null, null, new Date(2021, 12, 12), null);
+
+        cau1();
+        cau2(acc1);
+        cau3();
+    }
+
+    static void cau1() {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i += 2;
+        }
+    }
+
+    static void cau2(Account account) {
+        System.out.println(account);
+    }
+
+    static void cau3() {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
