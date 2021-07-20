0c6c9bc5e0c8f67bfcaa1039803a27311ae2a6e2
cuongbuilh <cuongbuilh@gmail.com>
2021-07-20 20:19:51 +0700

    update assignment_3 ; ex_5

--- Java Core/Asignment/Testing_System/Assignment_1_2_3_4_5_6_7/src/main/java/asignment_3/Exercise_5.java
+++ Java Core/Asignment/Testing_System/Assignment_1_2_3_4_5_6_7/src/main/java/asignment_3/Exercise_5.java
@@ -10,21 +10,21 @@ import java.util.Date;
 
 public class Exercise_5 {
     public static void main(String[] args) {
-        Department dep1 = new Department(1,"mar");
-        Department dep2 = new Department(2,"mar2");
-        Department dep3 = new Department(3,"mar");
+        Department dep1 = new Department(1, "mar");
+        Department dep2 = new Department(2, "mar2");
+        Department dep3 = new Department(3, "mar");
 
-        Position pos1 = new Position(1,"dev");
-        Position pos2 = new Position(2,"dev");
-        Position pos3 = new Position(3,"dev");
+        Position pos1 = new Position(1, "dev");
+        Position pos2 = new Position(2, "dev");
+        Position pos3 = new Position(3, "dev");
 
-        Group gr1 = new Group(1,"test");
-        Group gr2 = new Group(2,"test");
-        Group gr3 = new Group(3,"test");
+        Group gr1 = new Group(1, "test");
+        Group gr2 = new Group(2, "test");
+        Group gr3 = new Group(3, "test");
 
-        Account acc1 = new Account(1,"a@gmail.com","a","nva",dep1,pos1,new Date(2021,12,12), new Group[]{gr1,gr3});
-        Account acc2 = new Account(1,"a@gmail.com","a","nva",dep1,pos1,new Date(2021,12,12), new Group[]{gr1,gr3});
-        Account acc3 = new Account(1,"a@gmail.com","a","nva",dep1,pos1,new Date(2021,12,12), new Group[]{gr1,gr3});
+        Account acc1 = new Account(1, "a@gmail.com", "a", "nva", dep1, pos1, new Date(2021, 12, 12), new Group[]{gr1, gr3});
+        Account acc2 = new Account(1, "a@gmail.com", "a", "nva", dep1, pos1, new Date(2021, 12, 12), new Group[]{gr1, gr3});
+        Account acc3 = new Account(1, "a@gmail.com", "a", "nva", dep1, pos1, new Date(2021, 12, 12), new Group[]{gr1, gr3});
 
 
 //        cau1(dep1);
@@ -32,7 +32,7 @@ public class Exercise_5 {
 //        cau3(dep1);
 //        cau4(dep1);
 //        cau5(new Department[]{dep1,dep2,dep3});
-        cau6();
+//        cau6();
         cau7();
     }
 
@@ -61,10 +61,64 @@ public class Exercise_5 {
     }
 
     static void cau6() {
+        Department d1 = new Department(1, "Accounting");
+        Department d2 = new Department(1, "Mar");
+        Department d3 = new Department(1, "Sale");
+        Department d4 = new Department(1, "Bos");
+        Department d5 = new Department(1, "Wait");
+        Department[] departments = {d1, d2, d3, d3, d4, d5};
 
+        // sap xep mang;
+        for (int i = 0; i < departments.length - 1; i++) {
+            for (int j = i + 1; j < departments.length; j++) {
+                // lay cac ki tu dau
+                char fisrtCharDepI = departments[i].getDepartmentName().charAt(0);
+                char fisrtCharDepJ = departments[j].getDepartmentName().charAt(0);
+
+                if (fisrtCharDepI < fisrtCharDepJ) {
+                    Department temp = departments[i];
+                    departments[i] = departments[j];
+                    departments[j] = temp;
+                }
+            }
+        }
+
+        for (Department d : departments) {
+            System.out.println(d.getDepartmentName());
+        }
     }
 
     static void cau7() {
+        Department d1 = new Department(1, "Accounting");
+        Department d2 = new Department(1, "Mar");
+        Department d3 = new Department(1, "Sale A");
+        Department d4 = new Department(1, "Bos");
+        Department d5 = new Department(1, "Wait B");
+        Department[] departments = {d1, d2, d3, d3, d4, d5};
 
+        for (int i = 0; i < departments.length - 1; i++) {
+            for (int j = i + 1; j < departments.length; j++) {
+                // tach chuoi ra thanh cac tu
+                String[] tokensI = departments[i].getDepartmentName().split(" ");
+                String[] tokensJ = departments[j].getDepartmentName().split(" ");
+                // lay tu cuoi
+                String lastTokenI = tokensI[tokensI.length - 1];
+                String lastTokenJ = tokensJ[tokensJ.length - 1];
+                // lay chu cai dau
+                char firstCharI = lastTokenI.charAt(0);
+                char firstCharJ = lastTokenJ.charAt(0);
+
+                // doi vi tri
+                if (firstCharI > firstCharJ) {
+                    Department temp = departments[i];
+                    departments[i] = departments[j];
+                    departments[j] = temp;
+                }
+            }
+        }
+        // in danh sach
+        for (Department d: departments){
+            System.out.println(d.getDepartmentName());
+        }
     }
 }
